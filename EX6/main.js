355115512a453ff96842fe857453f964a68169c5
Nguyễn Đức Tư <62777860+nguyenductu0703@users.noreply.github.com>
2022-05-27 16:58:35 +0700

    commit

---
+++ EX6/main.js
@@ -0,0 +1,10 @@
+var StringOne = "Thực tập sinh reactJS tại LU, thực tập 2 tháng";
+var StringTwo = " tập ";
+// check contains string
+console.log(StringOne.includes(StringTwo))
+//find the starting position of the string
+console.log(StringOne.indexOf(StringTwo))
+//find the end position of the string
+console.log(StringOne.lastIndexOf(StringTwo))
+//find the number of occurrences of the string
+console.log(StringOne.split(StringOne).length)
