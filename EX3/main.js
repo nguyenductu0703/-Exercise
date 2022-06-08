841c24471fd09dcbf784597bf57d218634c15aa1
Nguyễn Đức Tư <62777860+nguyenductu0703@users.noreply.github.com>
2022-06-08 11:46:10 +0700

    commit

--- EX3/main.js
+++
@@ -1,9 +0,0 @@
-var values = document.querySelectorAll(".values");
-for (var i = 0; i < values.length; i++) {
-    var ID = values[i].querySelector(".ID");
-    if (ID.textContent % 2 === 0) {
-        values[i].className = 'background'
-    } else {
-        values[i].className = 'backgrounds'
-    }
-}
